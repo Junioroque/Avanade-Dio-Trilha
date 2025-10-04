784ce3839048064d1b97f4075a7cd057798487a2
Junio Roque <jcjunioroque@gmail.com>
2025-10-03 22:19:58 -0300

    and e or

---
+++ operadorLogico/Program.cs
@@ -0,0 +1,26 @@
+﻿//Operador or
+bool ehMaiorDeidade = false;
+bool possuiAurorizacaoDoResponsavel = false;
+
+Console.WriteLine("--------or---------");
+
+if(ehMaiorDeidade || possuiAurorizacaoDoResponsavel ) {
+   Console.WriteLine("Entrada liberada");
+} else {
+    Console.WriteLine("Entrada não liberada");
+}
+
+Console.WriteLine("--------and---------");
+//Operador and
+
+bool possuiPresençaMinima = true;
+ 
+double nota = 7.5;
+
+if(possuiPresençaMinima && nota >= 7.5) {
+    
+}
+
+
+
+
