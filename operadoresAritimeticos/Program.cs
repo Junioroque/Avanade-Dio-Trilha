b2d6e0e7d43e70f23d57ce4830fe6671d6ad36fc
Junio Roque <jcjunioroque@gmail.com>
2025-10-05 20:05:46 -0300

    Operadores aritimeticos

---
+++ operadoresAritimeticos/Program.cs
@@ -0,0 +1,19 @@
+﻿using operadoresAritimeticos;
+
+Calculadora calc = new Calculadora();
+calc.Somar(40, 2);
+calc.Subtrair(10,3);
+calc.Multiplicar(3, 22);
+calc.Dividir(100, 20);
+
+Console.WriteLine("------------Potenciação---------------");
+calc.Potencia(5, 5);
+
+Console.WriteLine("------------Seno---------------");
+calc.Seno(20);
+
+Console.WriteLine("------------Coseno---------------");
+calc.Coseno(20);
+
+Console.WriteLine("------------Tangente---------------");
+calc.Tangente(20);
