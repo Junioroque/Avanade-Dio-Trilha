3e01dfbeb32b3844a8e50526148ad2ee49a71e5e
Junio Roque <jcjunioroque@gmail.com>
2025-10-06 18:57:27 -0300

    laço de repetiçao

---
+++ estruturaRepeticao/Program.cs
@@ -0,0 +1,9 @@
+﻿int numero = 10;
+
+Console.WriteLine($"{numero} * 1 = {10 * 1}");
+
+Console.WriteLine("-----------For---------------");
+
+for(int contador = 0; contador <= 10; contador ++){
+    Console.WriteLine($"{contador} * {numero} = {contador * numero}");
+}
