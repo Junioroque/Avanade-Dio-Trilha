86e97504181ab0e1d8772c363cea05cc390f94d2
Junio Roque <jcjunioroque@gmail.com>
2025-10-06 20:58:58 -0300

    while

--- estruturaRepeticao/Program.cs
+++ estruturaRepeticao/Program.cs
@@ -1,4 +1,4 @@
-﻿int numero = 10;
+﻿int numero = 4;
 
 Console.WriteLine($"{numero} * 1 = {10 * 1}");
 
@@ -7,3 +7,13 @@ Console.WriteLine("-----------For---------------");
 for(int contador = 0; contador <= 10; contador ++){
     Console.WriteLine($"{contador} * {numero} = {contador * numero}");
 }
+
+Console.WriteLine("-----------while---------------");
+int cont = 4;
+while(cont <= 50){
+    Console.Write(cont);
+    Console.Write(" ");
+    cont++;
+}
+
+Console.WriteLine("-----------Do while---------------");
--- estruturaRepeticao/obj/Debug/net9.0/estruturaRepeticao.AssemblyInfo.cs
+++ estruturaRepeticao/obj/Debug/net9.0/estruturaRepeticao.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("estruturaRepeticao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a56f2ea2dacf10dddc90e02b2b942d884b142a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e01dfbeb32b3844a8e50526148ad2ee49a71e5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("estruturaRepeticao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("estruturaRepeticao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
